26249af06b2d50e94614d06a9724ec8e96edbe58
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-12-10 18:15:54 -0300

    feat: content

---
+++ 2-condicionais/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program {
+  public static void Main() {
+    Program.ForEachExample();
+  }
+
+  public static void ForEachExample() {
+    string[] names = new string[] {"Hulk", "Thor", "Loki"};
+
+    foreach( var name in names) {
+      Console.WriteLine(name);
+    }
+  }
+}
