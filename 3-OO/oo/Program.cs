3518d9a943a84822310667039155883509f93cbf
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2023-12-27 17:32:17 -0300

    feat: material de OO

---
+++ 3-OO/oo/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+  public int Num {
+    get { return Num; }
+    set {
+      if (value > 0) {
+        Num = value;
+      }
+    }
+  }
+
+  public static void Main() {
+    Program person = new Program() {
+      Num = 10
+    };
+    
+
+    Console.WriteLine(person.Num);
+  }
+}
